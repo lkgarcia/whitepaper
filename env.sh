aa2bec7d26a5dbd141fd7dd7e471e86a367c9b4a
Kevin <nicolo.kevin@gmail.com>
2025-11-17 00:11:25 +0800

    Update deployment

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export USE_SSH=true
 export GIT_USER="lkgarcia"
-export GIT_PASS="$1"
+export GIT_PASS="$(cat token.txt)"
 export CURRENT_BRANCH="main"
 export GIT_USER_NAME="lkgarcia"
 export GIT_USER_EMAIL="nicolo.kevin@gmail.com"
